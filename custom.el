d086803b9707a596dce67a2c8e0bc2d270cef54c
ykumards <ykumar@nyu.edu>
2022-11-06 13:41:08 +0200

    dabble with themes a bit more

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("636b135e4b7c86ac41375da39ade929e2bd6439de8901f53f88fde7dd5ac3561" "1cae4424345f7fe5225724301ef1a793e610ae5a4e23c023076dc334a9eb940a" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
+   '("0c08a5c3c2a72e3ca806a29302ef942335292a80c2934c1123e8c732bb2ddd77" "636b135e4b7c86ac41375da39ade929e2bd6439de8901f53f88fde7dd5ac3561" "1cae4424345f7fe5225724301ef1a793e610ae5a4e23c023076dc334a9eb940a" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- packages.el
+++ packages.el
@@ -10,6 +10,7 @@
 ;(package! some-package)
 
 (package! spacemacs-theme)
+(package! solarized-theme)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
